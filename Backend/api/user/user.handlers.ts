87117574d3880a68532d290d474ddd05e1906846
Michael Chen <mecow@umich.edu>
2025-04-23 14:01:26 -0400

    Add user handle for backend

--- Backend/api/user/user.handlers.ts
+++ Backend/api/user/user.handlers.ts
@@ -10,15 +10,15 @@ import { hashPassword } from "./user.middleware";
 import bcrypt from "bcrypt";
 import express, { Request, Response, NextFunction } from "express";
 import { errorHandleing } from "../../utils/helper";
-import { CreateUserEmail, CreateUser, } from "./user.type";
+import { CreateUserEmail, CreateUser } from "./user.type";
 import { authMethod } from "../../database/database.type";
 import { UserLoginInfo } from "../../database/database";
-import user from ".";
+import { generateJWT } from "./user.helper";
 
 export const getAllUser = async (
   req: Request<{}, {}, {}>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
     const user: UserLoginInfo[] = await get_all_user_db();
@@ -31,7 +31,7 @@ export const getAllUser = async (
 export const getAuthMethod = async (
   req: Request<{}, {}, {}>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
     const user: authMethod[] = await get_auth_method_db();
@@ -44,7 +44,7 @@ export const getAuthMethod = async (
 export const createUserThrGmail = async (
   req: Request<{}, {}, CreateUserEmail>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
     const { username, password, method, authId } = req.body;
@@ -55,14 +55,20 @@ export const createUserThrGmail = async (
       username,
       userOn,
       time,
-      password
+      password,
     );
 
     await connect_auth_method_db(user_login_info_id, method, authId);
-
     await connect_user_data_db(user_login_info_id);
 
-    res.status(201).send("User created successfully");
+    let user_data: UserLoginInfo = (await get_user_email_db(authId))!;
+    const { user_pass, ...userWithoutPass } = user_data;
+
+    const token: string = generateJWT(userWithoutPass);
+    res.status(201).json({
+      message: "User created successfully",
+      token: token,
+    });
   } catch (error) {
     errorHandleing(error, res, "createUserThrGmail");
   }
@@ -71,17 +77,20 @@ export const createUserThrGmail = async (
 export const createUser = async (
   req: Request<{}, {}, CreateUser>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
-    const { username, method, authId } = req.body;
+    const { authId, method, username } = req.body;
     const userOn: true = true;
     const time: Date = new Date();
 
-    const user_login_info_id: number = await create_user_db(username, userOn, time);
+    const user_login_info_id: number = await create_user_db(
+      username,
+      userOn,
+      time,
+    );
 
     await connect_auth_method_db(user_login_info_id, method, authId);
-
     await connect_user_data_db(user_login_info_id);
 
     res.status(201).send("User created successfully");
@@ -90,28 +99,40 @@ export const createUser = async (
   }
 };
 
+// login with email
 export const checkPasswordGmail = async (
   req: Request,
-  res: Response
+  res: Response,
 ): Promise<void> => {
   try {
-    const password: string = req.body.password;
-    const email: string = req.body.authId;
-    let user_data: UserLoginInfo = await get_user_email_db(email);
-    const isMatch: boolean = await bcrypt.compare(
-      password,
-      user_data.user_pass as string
-    );
-    console.log(user_data);
+    const { password, authId: email } = req.body; // Destructure email and password directly
+    let user_data = await get_user_email_db(email);
+    const userOrPassError = () => res.status(401).json({
+      error: true,
+      message: "username or password is incorrect.",
+      code: 401,
+    });
 
-    user_data = Object.fromEntries(
-      Object.entries(user_data).filter(([key, value]) => key != "user_pass")
-    ) as Omit<UserLoginInfo, "user_pass">;
-
-    if (isMatch) {
-      res.status(200).json(user_data);
+    if (!user_data) {
+      userOrPassError();
       return;
     }
+    const isMatch: boolean = await bcrypt.compare(
+      password,
+      user_data.user_pass as string,
+    );
+
+    if (!isMatch) {
+      userOrPassError();
+      return;
+    }
+    const { user_pass, ...userWithoutPass } = user_data;
+
+    const token = generateJWT(userWithoutPass);
+    res.status(201).json({
+      message: "Login successfully",
+      token,
+    });
   } catch (error) {
     errorHandleing(error, res, "checkPasswordGmail");
   }
